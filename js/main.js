7a5858e107f7b5569a349e79f7b70d869cca37e4
messiasab <messiasab@gmail.com>
2024-12-08 18:55:32 -0300

    Primeiro commit com meus documentos

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const robo= document.querySelector('#robo')
+robo.addEventListener("click",()=>{
+    console.log('cliquei no robo')
+})
+
+function dizoi(nome){
+    console.log("oi "+nome)
+    console.log('bem vindo')
+}
+dizoi()
